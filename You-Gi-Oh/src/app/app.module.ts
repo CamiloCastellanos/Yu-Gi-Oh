18098d41b53ed1218737e48a7ec16f0abafce346
CamiloCastellanos <camilo.castellanos.puentes@gmail.com>
2021-10-02 07:58:59 -0500

    Creacion de Paginas "Home","Price"

--- You-Gi-Oh/src/app/app.module.ts
+++ You-Gi-Oh/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './Pages/Home/Components/home/home.component';
+import { PriceComponent } from './Pages/Price/Components/price/price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
