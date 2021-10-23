862a48b78de2a7f70a2d3dc60d1c97e0ae00fab5
CamiloCastellanos <camilo.castellanos.puentes@gmail.com>
2021-10-22 20:42:27 -0500

    Creacion funcion buscar cartas

--- You-Gi-Oh/src/app/Core/Services/card.service.ts
+++ You-Gi-Oh/src/app/Core/Services/card.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CardInfo } from '../../Model/CardInfo/CardInfo';
+import { Observable, pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+
+
+
+  public SharedCard(nameCard: String):Observable<CardInfo>{
+    return this.httpclient.get<CardInfo>(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${nameCard}`)
+  }
+
+
+
+
 }
